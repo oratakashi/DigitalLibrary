10d722c510042ea7f0041e6616140b685ede00d4
Oratakashi <orata@oratateam.com>
2018-05-14 15:53:42 +0700

    penambahan

--- proyek/asset/content/slider.php
+++ proyek/asset/content/slider.php
@@ -5,7 +5,6 @@
 					<p>Kini tak perlu pergi jauh ke toko ataupun perpustakaan, dengan Digital Library baca buku jadi lebih mudah</p>
 					</div>
 					<img src="images/1.png" />
-					<img src="../images/1.png" />
 				</div>
 				<div class="step" data-color="color-3" data-y="900" data-rotate-x="80">
 					<div class="jms-content">
@@ -13,7 +12,6 @@
 					<p>Bosan antre dalam perpustakaan hanya untuk meminjam buku? , kini sudah tidak perlu antri lagi</p>
 					</div>
 					<img src="images/2.png" />
-					<img src="../images/2.png" />
 				</div>
 				<div class="step" data-color="color-4" data-x="-100" data-z="1500" data-rotate="170">
 					<div class="jms-content">
@@ -21,7 +19,6 @@
 					<p>Bingung anda tak punya lemari untuk menyimpan buku? kini Digital Library dapat jadi solusi</p>
 					</div>
 					<img src="images/3.png" />
-					<img src="../images/3.png" />
 				</div>
 				<div class="step" data-color="color-5" data-x="3000">
 					<div class="jms-content">
@@ -30,7 +27,6 @@
 						<a class="jms-link" href="#">Baca Sekarang</a>
 					</div>
 					<img src="images/4.png" />
-					<img src="../images/4.png" />
 				</div>
 				<div class="step" data-color="color-1" data-x="4500" data-z="1000" data-rotate-y="45">
 					<div class="jms-content">
@@ -38,6 +34,5 @@
 					<p>Tak punya waktu lebih untuk ke perpustakaan? dengan Digital Library anda bisa hemat waktu anda</p>
 					</div>
 					<img src="images/5.png" />
-					<img src="../images/5.png" />
 				</div>
 			</section>
